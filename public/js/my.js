a367e9ac64f1f0f9cea9ea6604b786fb0c32adaf
Larisa <32453313+larisaai@users.noreply.github.com>
2019-06-04 23:09:51 +0200

    Delete my.js

--- public/js/my.js
+++
@@ -1,9 +0,0 @@
-const floatLabels = document.querySelectorAll(".floatLabel");
-floatLabels.forEach(floatLabel => {
-  floatLabel.addEventListener("focus", () => {
-    floatLabel.nextElementSibling.className = "active";
-  });
-  // floatLabel.addEventListener("blur", () => {
-  //   floatLabel.nextElementSibling.className = "";
-  // });
-});
