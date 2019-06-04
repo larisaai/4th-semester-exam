85e7228636ee14ace710e1eae9c72691a6c156e7
Larisa <32453313+larisaai@users.noreply.github.com>
2019-06-04 23:35:02 +0200

    Display reviews from db in frontend

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,16 @@
+const reviewsDiv = document.querySelector(".reviews");
+
 fetch("/data")
   .then(res => res.json())
-  .then(data => console.log("data => ", data))
+  .then(data => {
+    data = JSON.parse(data);
+    data.forEach(item => {
+      const h1 = document.createElement("h1");
+      const p = document.createElement("p");
+      h1.innerHTML = item.name;
+      p.innerHTML = item.guestReview;
+      reviewsDiv.appendChild(h1);
+      reviewsDiv.appendChild(p);
+    });
+  })
   .catch(err => console.log(err));
