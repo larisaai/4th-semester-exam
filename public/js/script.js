1c796342f6a69405e09804c5721493a9efbba07e
Larisa <ailisoaielarisa@yahoo.com>
2019-06-05 19:26:16 +0200

    pop up cards javascript working

--- public/js/script.js
+++ public/js/script.js
@@ -9,15 +9,47 @@ fetch("/who")
       card.className = "introduction-card";
       const img = document.createElement("img");
       const cardTitle = document.createElement("h3");
-      const button = document.createElement("button");
+      const buttonDetails = document.createElement("button");
+      buttonDetails.className = "read-more-btn";
+      const readMoreArrow = document.createElement("div");
+      readMoreArrow.classList = "read-more-arrow";
+      const readMoreDetails = document.createElement("div");
+      readMoreDetails.className = "read-more-details";
+
+      buttonDetails.addEventListener("click", function(event) {
+        const card = document.querySelector(".introduction-card.active");
+        const currentCard = event.target.parentNode;
+        console.log(currentCard.classList.contains("active"));
+        if (currentCard.classList.contains("active")) {
+          currentCard.classList.remove("active");
+        } else {
+          event.target.parentNode.classList.add("active");
+        }
+
+        if (card) {
+          card.classList.remove("active");
+        }
+      });
+
+      const details1 = document.createElement("div");
+      const details2 = document.createElement("div");
+      const imgInfo = document.createElement("img");
       img.src = item.icon;
       cardTitle.innerHTML = item.title;
-      button.innerHTML = "Read more";
+      buttonDetails.innerHTML = "Read more";
+      details1.innerHTML = item.firstElement;
+      details2.innerHTML = item.secondElement;
+      imgInfo.src = item.imgDetails;
 
       card.appendChild(cardTitle);
       card.appendChild(img);
-      card.appendChild(button);
+      card.appendChild(buttonDetails);
+      buttonDetails.appendChild(readMoreArrow);
       whoIsUs.appendChild(card);
+      readMoreDetails.appendChild(details1);
+      readMoreDetails.appendChild(details2);
+      readMoreDetails.appendChild(imgInfo);
+      card.appendChild(readMoreDetails);
     });
   })
   .catch(err => console.log(err));
