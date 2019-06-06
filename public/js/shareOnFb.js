f60eda58a40a295752dde1b50fba7d2726169076
Larisa <ailisoaielarisa@yahoo.com>
2019-06-06 20:41:56 +0200

    fix a lot of problems

--- public/js/shareOnFb.js
+++ public/js/shareOnFb.js
@@ -1,7 +1,18 @@
-var fbButton = document.getElementById("fb-share-button");
-var url = window.location.href;
+let fbButton = document.getElementById("fb-share-button");
+let url = window.location.href;
 
 fbButton.addEventListener("click", function() {
+  window.open(
+    "https://www.facebook.com/sharer/sharer.php?u=" + url,
+    "facebook-share-dialog",
+    "width=800,height=600"
+  );
+  return false;
+});
+
+let fbButtonFooter = document.getElementById("fb-share-button-footer");
+
+fbButtonFooter.addEventListener("click", function() {
   window.open(
     "https://www.facebook.com/sharer/sharer.php?u=" + url,
     "facebook-share-dialog",
