94237af877d089822a7d4dca2f55c6d52bb8dc77
Larisa <ailisoaielarisa@yahoo.com>
2019-06-06 16:19:19 +0200

    fix some general reusable clases

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,15 +1,33 @@
-const Images = document.querySelector(".gallery-img");
-Images.addEventListener("click", function(event) {
-  const image = document.querySelector(".gallery-img.active");
-  const currentCard = event.target.parentNode;
-  console.log(currentCard.classList.contains("active"));
-  if (currentCard.classList.contains("active")) {
-    currentCard.classList.remove("active");
-  } else {
-    event.target.parentNode.classList.add("active");
-  }
+let galleryImages = document.querySelectorAll("#gallery img");
+let galleryImages2 = document.querySelectorAll("#gallery2 img");
 
-  if (card) {
-    card.classList.remove("active");
-  }
-});
+for (i = 0; i < galleryImages.length; i++) {
+  galleryImages[i].addEventListener("click", function() {
+    const image = document.querySelector(".active .gallery-img");
+    const currentImg = event.target.parentNode;
+    console.log(currentImg.classList.contains("active"));
+    if (currentImg.classList.contains("active")) {
+      currentImg.classList.remove("active");
+    } else {
+      event.target.parentNode.classList.add("active");
+    }
+    if (image) {
+      image.classList.remove("active");
+    }
+  });
+}
+for (i = 0; i < galleryImages2.length; i++) {
+  galleryImages2[i].addEventListener("click", function() {
+    const image2 = document.querySelector(".active .gallery-img");
+    const currentImg2 = event.target.parentNode;
+    console.log(currentImg.classList.contains("active"));
+    if (currentImg2.classList.contains("active")) {
+      currentImg2.classList.remove("active");
+    } else {
+      event.target.parentNode.classList.add("active");
+    }
+    if (image2) {
+      image2.classList.remove("active");
+    }
+  });
+}
