2248a6e1e28e268aa7a339f2cc671e56666d1af8
Larisa <ailisoaielarisa@yahoo.com>
2019-06-06 23:48:03 +0200

    work in progress

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,33 +1,33 @@
-let galleryImages = document.querySelectorAll("#gallery img");
-let galleryImages2 = document.querySelectorAll("#gallery2 img");
+// let galleryImages = document.querySelectorAll("#gallery img");
+// let galleryImages2 = document.querySelectorAll("#gallery2 img");
 
-for (i = 0; i < galleryImages.length; i++) {
-  galleryImages[i].addEventListener("click", function() {
-    const image = document.querySelector(".active .gallery-img");
-    const currentImg = event.target.parentNode;
-    console.log(currentImg.classList.contains("active"));
-    if (currentImg.classList.contains("active")) {
-      currentImg.classList.remove("active");
-    } else {
-      event.target.parentNode.classList.add("active");
-    }
-    if (image) {
-      image.classList.remove("active");
-    }
-  });
-}
-for (i = 0; i < galleryImages2.length; i++) {
-  galleryImages2[i].addEventListener("click", function() {
-    const image2 = document.querySelector(".active .gallery-img");
-    const currentImg2 = event.target.parentNode;
-    console.log(currentImg.classList.contains("active"));
-    if (currentImg2.classList.contains("active")) {
-      currentImg2.classList.remove("active");
-    } else {
-      event.target.parentNode.classList.add("active");
-    }
-    if (image2) {
-      image2.classList.remove("active");
-    }
-  });
-}
+// for (i = 0; i < galleryImages.length; i++) {
+//   galleryImages[i].addEventListener("click", function() {
+//     const image = document.querySelector(".active .gallery-img");
+//     const currentImg = event.target.parentNode;
+//     console.log(currentImg.classList.contains("active"));
+//     if (currentImg.classList.contains("active")) {
+//       currentImg.classList.remove("active");
+//     } else {
+//       event.target.parentNode.classList.add("active");
+//     }
+//     if (image) {
+//       image.classList.remove("active");
+//     }
+//   });
+// }
+// for (i = 0; i < galleryImages2.length; i++) {
+//   galleryImages2[i].addEventListener("click", function() {
+//     const image2 = document.querySelector(".active .gallery-img");
+//     const currentImg2 = event.target.parentNode;
+//     console.log(currentImg.classList.contains("active"));
+//     if (currentImg2.classList.contains("active")) {
+//       currentImg2.classList.remove("active");
+//     } else {
+//       event.target.parentNode.classList.add("active");
+//     }
+//     if (image2) {
+//       image2.classList.remove("active");
+//     }
+//   });
+// }
