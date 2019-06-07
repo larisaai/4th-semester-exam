c10c9a735ff58af64fe19a6ffdea469b8c1ec814
Larisa <ailisoaielarisa@yahoo.com>
2019-06-07 06:22:49 +0200

    make the website responsive

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,40 +1,3 @@
-// //toggle dropdown menu open/close
-// let toClose = false;
-
-// function toggle(e) {
-//   e.stopPropagation();
-//   let btn = this;
-//   let menu = btn.nextSibling;
-
-//   while (menu && menu.nodeType != 1) {
-//     menu = menu.nextSibling;
-//   }
-//   if (!menu) return;
-//   if (menu.style.display !== "block") {
-//     menu.style.display = "block";
-//     if (toClose) toClose.style.display = "none";
-//     toClose = menu;
-//   } else {
-//     menu.style.display = "none";
-//     toClose = false;
-//   }
-// }
-// function closeAll() {
-//   toClose.style.display = "none";
-// }
-
-// window.addEventListener("DOMContentLoaded", function() {
-//   document.querySelectorAll(".menu-item-btn").forEach(function(btn) {
-//     btn.addEventListener("click", toggle, true);
-//   });
-// });
-
-// window.onclick = function(event) {
-//   if (toClose) {
-//     closeAll.call(event.target);
-//   }
-// };
-
 const dropdown = [...document.querySelectorAll(".menu-item-btn")].map(item => {
   item.addEventListener("click", e => {
     const currentMenuItem = e.target.parentNode;
@@ -50,4 +13,16 @@ const dropdown = [...document.querySelectorAll(".menu-item-btn")].map(item => {
       menuItem.classList.remove("active");
     }
   });
+});
+
+const burger = document.querySelector(".burger-menu");
+const parent = document.querySelector(".menu");
+
+burger.addEventListener("click", function() {
+  if (parent.classList.contains("open")) {
+    parent.classList.remove("open");
+  } else {
+    parent.classList.add("open");
+  }
+  // nav.className = !nav.classList.contains("active") ? "active" : "";
 });
